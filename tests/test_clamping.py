3076508cc421cf9ecf1d508b67e2820b76e35971
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-02-20 12:33:54 +0100

    Minor fixes

--- tests/test_clamping.py
+++ tests/test_clamping.py
@@ -1,29 +1,32 @@
 import pytest
 
-from FTracker.utils.clamping import string_to_float
+from FTracker.utils import clamping
 
 
 
 # ---- Correct Values ---- #
 
+# Test will pass if it doesn't raise an exception
+
 def test_with_correct_input_dot():
-    string_to_float("50.5")
+    clamping.string_to_float("50.5")
 
 def test_with_correct_input_comma():
-    string_to_float("33,33")
+    clamping.string_to_float("33,33")
 
 def test_with_correct_input_int_representation():
-    string_to_float("123")
+    clamping.string_to_float("123")
 
 
 # ---- Incorrect Values ---- #
+
 # Tests below take incorrect input and should raise an exception
 # 'TypeError' exception occurs when input argument is not of type str
 
 def test_with_incorrect_type_int():
     with pytest.raises(TypeError):
-        string_to_float(165)
+        clamping.string_to_float(165)
 
 def test_with_incorrect_type_float():
     with pytest.raises(TypeError):
-        string_to_float(12.75)
+        clamping.string_to_float(12.75)
